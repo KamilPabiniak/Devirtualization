67babcc4c2ec8784f401c4917f14a4f84b01a7b5
MellStone <92853741+MellStone@users.noreply.github.com>
2025-01-17 06:58:39 +0100

    CurveRoomShader

---
+++ Assets/DenysShaders/RoomShadeController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomShadeController : MonoBehaviour
+{
+    [SerializeField] private Material shader;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
