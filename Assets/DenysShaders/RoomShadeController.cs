9b5db6e80165b57b6f9c5061acc0b22d7443b4db
MellStone <92853741+MellStone@users.noreply.github.com>
2025-01-19 11:30:51 +0100

    Weird Shader done

--- Assets/DenysShaders/RoomShadeController.cs
+++ Assets/DenysShaders/RoomShadeController.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 
 public class RoomShadeController : MonoBehaviour
 {
+    [SerializeField] private float limit;
+    [SerializeField] private float posZ;
+    [SerializeField] private float offset;
+
     [SerializeField] private Material shader;
 
 
     private void Start()
     {
-        
+        limit = shader.GetFloat("_Mask2");
+
     }
 
     private void Update()
     {
+        posZ = transform.position.z / 10f;
+        if (posZ >= -limit && posZ <= limit)
+        {
+            Debug.Log("Working");
+            shader.SetFloat("_Mask1", posZ + offset);
+        }
         
     }
 }
